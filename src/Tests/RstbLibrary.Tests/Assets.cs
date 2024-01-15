7f578fb072a4ea6236c5791f73443f6bd15627d2
ArchLeaders <archleadership28@gmail.com>
2024-01-14 22:07:19 -0800

    Create simple tests

---
+++ src/Tests/RstbLibrary.Tests/Assets.cs
@@ -0,0 +1,10 @@
+﻿namespace RstbLibrary.Tests;
+
+public static class Assets
+{
+    public static readonly byte[] RstbData = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Assets", "Rstb.test"));
+    public static readonly Rstb Rstb = Rstb.FromBinary(RstbData);
+
+    public static readonly byte[] RestblData = File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "Assets", "Restbl.test"));
+    public static readonly Rstb Restbl = Rstb.FromBinary(RestblData);
+}
---
+++ src/Tests/RstbLibrary.Tests/OperationsTests.cs
@@ -0,0 +1,50 @@
+﻿using Revrs;
+
+namespace RstbLibrary.Tests;
+
+public class OperationsTests
+{
+    [Fact]
+    public void ValidateReadRstb()
+    {
+        Rstb rstb = Rstb.FromBinary(Assets.RstbData);
+        rstb.Version.Should().Be(RstbVersion.Fixed);
+        rstb.Endianness.Should().Be(Endianness.Little);
+        rstb.HashTable.Count.Should().Be(65816);
+        rstb.OverflowTable.Count.Should().Be(2);
+    }
+
+    [Fact]
+    public void ValidateReadRestbl()
+    {
+        Rstb restbl = Rstb.FromBinary(Assets.RestblData);
+        restbl.Version.Should().Be(RstbVersion.Dynamic);
+        restbl.Endianness.Should().Be(Endianness.Little);
+        restbl.HashTable.Count.Should().Be(379715);
+        restbl.OverflowTable.Count.Should().Be(32);
+    }
+
+    [Fact]
+    public void ValidateWriteRstb()
+    {
+        byte[] rstbData = Assets.Rstb.ToBinary();
+
+        Rstb rstb = Rstb.FromBinary(rstbData);
+        rstb.Version.Should().Be(RstbVersion.Fixed);
+        rstb.Endianness.Should().Be(Endianness.Little);
+        rstb.HashTable.Count.Should().Be(65816);
+        rstb.OverflowTable.Count.Should().Be(2);
+    }
+
+    [Fact]
+    public void ValidateWriteRestbl()
+    {
+        byte[] restblData = Assets.Restbl.ToBinary();
+
+        Rstb restbl = Rstb.FromBinary(restblData);
+        restbl.Version.Should().Be(RstbVersion.Dynamic);
+        restbl.Endianness.Should().Be(Endianness.Little);
+        restbl.HashTable.Count.Should().Be(379715);
+        restbl.OverflowTable.Count.Should().Be(32);
+    }
+}
